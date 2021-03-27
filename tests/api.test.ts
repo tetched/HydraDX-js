05771233b177799ea0c90934e2cdb6740d3bf65e
smupyknight <smupyknight@outlook.com>
2021-03-27 14:53:46 +0100

    Fix

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,9 +1,9 @@
 import Api from '../src/api';
 import { HydraApiPromise } from '../src/types';
+import { getAliceAccount } from './utils/getAliceAccount';
 
 let api: HydraApiPromise;
-let HydraAccount = '5EKq4zjH8skHctyTS5QGaQYQnw3xyPW6cqkAPLPhwiap62DQ';
-let assetList;
+let aliceAccount: string;
 
 test('API object should be undefined at first', () => {
   api = Api.getApi();
@@ -12,21 +12,22 @@ test('API object should be undefined at first', () => {
 
 it('API object should be defined after initialize', async () => {
   api = await Api.initialize({}, process.env.WS_URL);
+  aliceAccount = getAliceAccount().address;
   expect(api).toBeDefined();
 });
 
 test('Test getAccountBalances', async () => {
-  const accountBalances = await api.hydraDx.query.getAccountBalances(HydraAccount);
+  const accountBalances = await api.hydraDx.query.getAccountBalances(aliceAccount);
   expect(accountBalances).toBeDefined();
 });
 
 test('Test getAssetList', async () => {
-  assetList = await api.hydraDx.query.getAssetList(HydraAccount);
+  const assetList = await api.hydraDx.query.getAssetList(aliceAccount);
   expect(assetList).toBeDefined();
 });
 
 test('Test getPoolInfo', async () => {
-  const poolInfo = await api.hydraDx.query.getPoolInfo(HydraAccount);
+  const poolInfo = await api.hydraDx.query.getPoolInfo(aliceAccount);
   expect(poolInfo).toBeDefined();
 });
 
